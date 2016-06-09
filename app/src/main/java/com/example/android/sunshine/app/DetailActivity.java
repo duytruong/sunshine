99e44a08c6025e273252518c7dee7e181c47dc92
truongducduy <ducduytruong2012@gmail.com>
2016-06-10 00:16:51 +0700

    Init project

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.detail_container, new DetailFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class DetailFragment extends Fragment {
+
+        private TextView tvDetailForecast;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String detailForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                tvDetailForecast = (TextView) rootView.findViewById(R.id.textview_detail_forecast);
+                tvDetailForecast.setText(detailForecast);
+            }
+            return rootView;
+        }
+    }
+}
